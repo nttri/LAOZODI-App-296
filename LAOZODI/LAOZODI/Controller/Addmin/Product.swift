411b07e78be703c0c9ebba978dd759626120c766
nttri <bem0302@gmail.com>
2018-07-02 09:06:58 +0700

    [bbb] edit product.swift

--- LAOZODI/LAOZODI/Controller/Addmin/Product.swift
+++ LAOZODI/LAOZODI/Controller/Addmin/Product.swift
@@ -16,27 +16,27 @@ class Product{
     var brand : String
     var strURLImage : String
     var warranty: Int
-    var abc = "queen and i am"
+    var xyz = "product from branch"
     
     init(Identifier _identifier :Int, Name _name: String, Description _description:String, Price _price: Int,
          Brand _brand:String, StrURLImage _strURLImage: String, Warranty _waranty: Int){
-        self.identifier = 60
+        self.identifier = _identifier
         self.name = _name
         self.description = _description
-        self.price = 100_000
+        self.price = _price
         self.brand = _brand
         self.strURLImage = _strURLImage
         self.warranty = _waranty
     }
     
     init(){
-        self.identifier = 100
-        self.name = ".     "
-        self.description = ".       "
-        self.price = 10_000
-        self.brand = ".       "
-        self.strURLImage = ".     "
-        self.warranty = 3
+        self.identifier = -1
+        self.name = "nil"
+        self.description = "nil"
+        self.price = -1
+        self.brand = "nil"
+        self.strURLImage = "nil"
+        self.warranty = -1
     }
     
     
